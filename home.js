c57ae18d8157505d7884c6d8909a88d0b5c0879b
Adil4961 <adilofficial.4961@gmail.com>
2023-02-07 21:01:39 +0530

    add new fie

---
+++ home.js
@@ -0,0 +1,109 @@
+//exercise 07
+
+//1.
+// for(let x=1;x<=100;x++){
+//     console.log(x)
+// }
+
+
+//2.
+// for(a = 2;a<=100;a++){
+//     console.log("even  "+ a)
+//     a++
+//}
+
+//for(a = 2;a<=100;a++)
+// if(a%2===0){
+//     console.log("even "+a)
+// }
+
+
+//3.
+// for(let a=1;a<=100;a++)
+// if(a%2===1){
+// console.log("odd "+a)
+// }
+        
+// for(a=1;a<=10;a++)
+//     console.log(a+a)
+
+// for(let a=1;a<=100;a=a+2)
+// console.log("odd "+a)
+
+ 
+//4. 
+// for(let a=5;a<=100;a=a+5){
+//     console.log("multiple of five "+a)
+// }
+ 
+// for(let x=1;x<=100;x++)
+// if(x%5==0){
+//     console.log("multiple of five"+x)
+// }
+
+
+ //5.
+// for(let i=1;i<=100;i++)
+// if(i%5===0&&i%15===0){
+//     console.log("multiple of five & fifteen "+i)
+// }
+
+
+//6.
+// for(let i=2;i<=20;i=i+2){
+//     console.log(i)
+// }
+
+// for(let i=2;i<=20;i++){
+//     console.log(i)
+//     i++
+//}
+
+// for(let i=3;i<=30;i=i+3)// table of three
+// console.log(i)
+
+// for(let i=3;i<=30;i++)
+// if(i%3===0){
+//     console.log(i) table of three
+// }
+ 
+// for(let i=4;i<=40;i=i+4)// table of 4
+// console.log(i)
+
+// for(let i=6;i<=60;i++)
+// if(i%6===0){
+//     console.log(i)// table of 6
+// }
+
+
+//7.
+// let user = Number(prompt("how many times do you want to print hello world"))
+// for(let i = 1;i<=user;i++)
+// console.log("hello world")
+
+
+//8.
+// let x = 0
+// for(let a=1;a<=100;a++){
+// x = x+a
+// };
+// console.log(x)
+
+
+//9.
+// let a = 0
+// for(let b = 1;b<=100;b++)
+// if(b%2===0){
+// a=a+b
+// }
+// console.log("even "+a)
+
+
+//10.
+// let a = 0;
+// for(let b = 1;b<=100;b++)
+// if(b%2===1){
+// a=a+b
+// }
+// console.log("odd "+a)
+
